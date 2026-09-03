a88eb73551ef7320f53d2c4e3b0f5b8fffedf4d5
AstwareDev <astwaredev@gmail.com>
2026-09-03 19:27:59 +0400

    Update CORS headers to include 'ngrok-skip-browser-warning' for improved API request handling

--- API/src/server.ts
+++ API/src/server.ts
@@ -28,7 +28,7 @@ app.use((req, res, next) => {
     res.setHeader('Vary', 'Origin')
   }
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, OPTIONS')
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Accept, X-Corro-Device')
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Accept, X-Corro-Device, ngrok-skip-browser-warning')
   res.setHeader('Access-Control-Expose-Headers', 'X-Corro-Device, X-Corro-Session')
   if (req.method === 'OPTIONS') {
     res.sendStatus(204)
